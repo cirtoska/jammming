93c3e2c4fba4f80e7bccdeaca327273f1a420b09
Sashka.Chi <cirtoska@gmail.com>
2022-11-16 12:52:22 +0100

    trying new things

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -34,12 +34,17 @@ class Track extends React.Component {
   render() {
     return (
       <div className="Track">
+        <img src={this.props.track.images} alt="" />
+        {console.log(this.props.track.images)}
         <div className="Track-information">
           <h3>{this.props.track.name}</h3>
           <p>
             {this.props.track.artist} | {this.props.track.album}
           </p>
         </div>
+        <audio className="audio-track" controls>
+          <source src={this.props.track.preview_url}></source>
+        </audio>
         {this.renderAction()}
       </div>
     );
